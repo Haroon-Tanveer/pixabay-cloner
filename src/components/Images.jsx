f5b53344db238600fb1a4785b382fb13bc48fc3a
Haroon Tanveer <192677006+Haroon-Tanveer@users.noreply.github.com>
2025-07-24 01:13:23 +0500

    kl

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import PixabayContext from '../context/pixabayContext'
+import PixabayContext from '../context/PixabayContext'
 
 
 const Images = () => {
--- src/context/PixabayState.jsx
+++ src/context/PixabayState.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import PixabayContext from './PixabayContext'
+import React, { useEffect, useState } from "react";
+import PixabayContext from "./PixabayContext";
 
 function PixabayState(props) {
-    const [imageData, setImageData] = useState([])
-    const [query, setQuery] = useState('paris')
+  const [imageData, setImageData] = useState([]);
+  const [query, setQuery] = useState("paris");
 
-    const api_key = "51436168-22ab8c9fc2775939b1fde1b71"
-    useEffect(() => {
-        const fetchData = async () => {
-            const api = await fetch(`https://pixabay.com/api/?key=${api_key}&q=${query}&image_type=photo&pretty=true&per_page=100`)
-            const data = await api.json()
-            setImageData(data.hits)
+  const api_key = "51436168-22ab8c9fc2775939b1fde1b71";
+  useEffect(() => {
+    const fetchData = async () => {
+      const api = await fetch(
+        `https://pixabay.com/api/?key=${api_key}&q=${query}&image_type=photo&pretty=true&per_page=100`
+      );
+      const data = await api.json();
+      setImageData(data.hits);
 
-            console.log(data.hits)
-        }
-        fetchData()
-    }, [])
+      console.log(data.hits);
+    };
+    fetchData();
+  }, []);
 
-    const fetchImageByCategory = async (cat) => {
-        const api = await fetch(`https://pixabay.com/api/?key=${api_key}&category=${cat}&image_type=photo&pretty=true&per_page=100`)
-        const data = await api.json()
-        setImageData(data.hits)
-        console.log(data.hits)
-    }
-    return (
-        <PixabayContext.Provider value={{
-            imageData, fetchImageByCategory, setQuery
-        }}> {props.children}</PixabayContext.Provider>
-    )
+  const fetchImageByCategory = async (cat) => {
+    const api = await fetch(
+      `https://pixabay.com/api/?key=${api_key}&category=${cat}&image_type=photo&pretty=true&per_page=100`
+    );
+    const data = await api.json();
+    setImageData(data.hits);
+    console.log(data.hits);
+  };
+  return (
+    <PixabayContext.Provider
+      value={{
+        imageData,
+        fetchImageByCategory,
+        setQuery,
+      }}
+    >
+      {" "}
+      {props.children}
+    </PixabayContext.Provider>
+  );
 }
 
-export default PixabayState
+export default PixabayState;
